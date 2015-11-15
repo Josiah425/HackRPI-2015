e8ccd3ff18c7a0fc0071c21413218ae8f2acb9d5
Josiah425 <Josiahb425@gmail.com>
2015-11-15 02:04:42 -0500

    Everything

--- HackRPI 2015/src/AbstractTest.java
+++ HackRPI 2015/src/AbstractTest.java
@@ -12,38 +12,58 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public abstract class AbstractTest {
-	
+
 	public static void test(WebDriver driver, String[] args){
-		
 		driver.get("https://bing.campuscardcenter.com/ch/login.html");
-        WebElement query = driver.findElement(By.name("username"));
-        query.sendKeys(args[0]);
-        WebElement query2 = driver.findElement(By.name("password"));
-        query2.sendKeys(args[1]);
-        query2.sendKeys(Keys.ENTER);
-        WebElement query3 = driver.findElement(By.cssSelector("div#content .feature table:nth-child(2) tr:nth-child(5)>td>a"));
-        query3.click();
-        WebElement query4 = driver.findElement(By.cssSelector("div#content .feature table"));
-        byte[] sodexoData = query4.getText().getBytes();
-        // TODO: Loop through each page
-        
+		WebElement query = driver.findElement(By.name("username"));
+		query.sendKeys(args[0]);
+		WebElement query2 = driver.findElement(By.name("password"));
+		query2.sendKeys(args[1]);
+		query2.sendKeys(Keys.ENTER);
+		WebElement query3 = driver.findElement(By.cssSelector("div#content .feature table:nth-child(2) tr:nth-child(5)>td>a"));
+		query3.click();
+
         // Output text into text file
         Path currentPath = Paths.get("");
         String path = currentPath.toAbsolutePath().toString();
         File fileOut = new File(path + "\\dataOutput2.txt");
         
-        
-        FileOutputStream out = null;
-        try {
+		FileOutputStream out = null;
+		try{
 			out = new FileOutputStream(fileOut);
-			out.write(sodexoData);
-			out.close();
-			System.out.println("----- File created. -----");
-		} catch (Exception e) {
+		}
+		catch(Exception e){
 
 		}
-        
-        parseData(fileOut);
+		WebElement pageNumber = driver.findElement(By.cssSelector("div#content .feature table tr a:last-child"));
+		String stringNumber = pageNumber.getAttribute("href");
+		String[] hyperLinkArray = stringNumber.split("=");
+		int lastPage = Integer.parseInt(hyperLinkArray[2]);
+		byte[][] sodexoData = new byte[lastPage][];
+
+		for (int i=0; i < lastPage-1; i++){
+			WebElement query4 = driver.findElement(By.cssSelector("div#content .feature table"));
+			sodexoData[i] = query4.getText().getBytes();
+			if (i == lastPage - 2){
+				WebElement pageTurner = driver.findElement(By.cssSelector("div#content .feature table tr a:last-child"));
+				pageTurner.click();
+			}
+			else if (i < lastPage-2){
+				WebElement pageTurner = driver.findElement(By.cssSelector("div#content .feature table tr a:nth-last-child(2)"));
+				pageTurner.click();
+			}
+		}
+		try{
+			for (int j=0; j<sodexoData.length; j++){
+				out.write(sodexoData[j]);
+			}
+			out.close();
+			System.out.println("----- File created. -----");
+		}
+		catch (Exception e){
+
+		}
+		parseData(fileOut);
 	}
 	
 	public static void parseData(File fileOut){
